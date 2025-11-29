331e9cb7de8b08706c2b2fd10cf255b55949377c
M Raihan H <ar937464@gmail.com>
2025-11-29 12:12:59 +0700

    fundamental

---
+++ public/fundamental.js
@@ -0,0 +1,16 @@
+// fundamental number
+let harga = 100000;
+let diskon = 1.4;
+let total = harga - (harga * diskon)
+let akar = Math.sqrt()
+console.log("Total=", total)
+console.log("akar=", akar)
+// string
+let nama = "m raihan";
+let panjangNama = nama.length;
+let namaKecil = nama.toUpperCase();
+let salam = "selamat pagi".replace("pagi", "sore");
+// bolean
+let nilai = 70;
+let isSelesai = true;
+let islulus = (nilai >= 70)
